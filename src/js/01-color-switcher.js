8bea6e24ce31a4436589751b4c2e94006aa64cbb
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-09 18:40:46 +0200

    complete color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+const TIME_DELAY = 1000;
+let intervalId = null;
 
+setDisabled(refs.stopBtn);
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick(e) {
+  const sibling = e.target.nextElementSibling;
+  setDisabled(e.target);
+  removeDisabled(sibling);
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, TIME_DELAY);
+}
+function onStopClick(e) {
+  const sibling = e.target.previousElementSibling;
+  setDisabled(e.target);
+  removeDisabled(sibling);
+  clearInterval(intervalId);
+}
+
+function setDisabled(elem) {
+  elem.setAttribute('disabled', !elem.disabled);
+}
+function removeDisabled(elem) {
+  elem.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
